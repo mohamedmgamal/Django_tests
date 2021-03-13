27b2d8a85de3f6d8859c92863380e8bb79fd7a38
mohamedmgamal <mohamedmgamal_97@yahoo.com>
2021-03-13 17:59:22 +0200

    wrapping up 2

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import  views
 urlpatterns = [
     path('', views.empty, name='empty'),
-        path('viewAll',views.viewAll , name="viewAll"),
+        path('viewAll',views.viewAll , name="viewAllTasks"),
         path("addTask",views.addTask,name="addTask"),
         path('update/<int:id>', views.update, name="update"),
         path('delete/<int:id>',views.delete,name="delete"),
--- tasks/views.py
+++ tasks/views.py
@@ -7,17 +7,17 @@ def empty(request):
 def viewAll(request):
     tasks=Task.objects.all()
     print(tasks)
-    return render(request,"showAll.html",{
+    return render(request,"showAllTasks.html",{
         'tasks':tasks
     })
 def delete(request,id):
     Task.objects.get(pk=id).delete()
-    return redirect('viewAll')
+    return redirect('viewAllTasks')
 def addTask(request):
     if request.method=="POST":
         task=Task(title=request.POST["title"],description=request.POST["description"])
         task.save()
-        return redirect('viewAll')
+        return redirect('viewAllTasks')
     return render(request,'addTask.html')
 def update(request,id):
     task=Task.objects.get(pk=id)
@@ -25,7 +25,7 @@ def update(request,id):
         task.title=request.POST["title"]
         task.description=request.POST["description"]
         task.save()
-        return redirect('viewAll')
+        return redirect('viewAllTasks')
     return render(request,'update.html',{
         "task":task
     })
